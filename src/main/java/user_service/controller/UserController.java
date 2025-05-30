f2fcc9c13501a3e72d5dbea5ea01587c96993116
Ilhan Mustafa <ilhnmst@gmail.com>
2025-05-31 00:25:38 +0300

    change validate user controller

--- src/main/java/user_service/controller/UserController.java
+++ src/main/java/user_service/controller/UserController.java
@@ -1,12 +1,16 @@
 package user_service.controller;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import user_service.dto.UserAuthDto;
 import user_service.dto.UserCacheDto;
 
+@Validated
 public interface UserController { //TODO CREATE DOC
 
-    ResponseEntity<UserCacheDto> getUserCacheForFeed(long userId);
+    ResponseEntity<UserCacheDto> getUserCacheForFeed(@Min(value = 1, message = "should more than 0") long userId);
 
-    ResponseEntity<UserAuthDto> getUserAuth(String username);
+    ResponseEntity<UserAuthDto> getUserAuth(@NotBlank String username);
 }
--- src/main/java/user_service/controller/UserControllerImpl.java
+++ src/main/java/user_service/controller/UserControllerImpl.java
@@ -1,10 +1,7 @@
 package user_service.controller;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,22 +13,19 @@ import user_service.service.UserService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
-@Validated
 public class UserControllerImpl implements UserController {
-    private static final String INVALID_ID_MSG = "should more than 0";
 
     private final UserService userService;
 
     @GetMapping("/{userId}/cache")
     @Override
-    public ResponseEntity<UserCacheDto> getUserCacheForFeed(
-            @PathVariable @Min(value = 1, message = INVALID_ID_MSG) long userId) {
+    public ResponseEntity<UserCacheDto> getUserCacheForFeed(@PathVariable long userId) {
         return ResponseEntity.ok().body(userService.getUserCache(userId));
     }
 
     @GetMapping("/{userId}/auth")
     @Override
-    public ResponseEntity<UserAuthDto> getUserAuth(@PathVariable @NotBlank String username) {
+    public ResponseEntity<UserAuthDto> getUserAuth(@PathVariable String username) {
         return ResponseEntity.ok().body(userService.getUserAuth(username));
     }
 }
